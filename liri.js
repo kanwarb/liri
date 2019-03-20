88ec67fb18fd9b29dceb139f8577d609683d8492
KB Total Recall <kanwar@gmail.com>
2019-03-19 19:56:47 -0700

    Latest code Work in progress , Not ready to deploy

--- liri.js
+++ liri.js
@@ -1,50 +1,108 @@
 var liricfg = require('dotenv').config();
 var keys = require("./keys.js");
 var axios = require("axios");
-var inquirer = require("inquirer");
-var spotify = require("node-spotify-api");
+var Spotify = require("node-spotify-api");
+var moment  = require("moment");
+
+var myargs = process.argv;
+var myChoice = process.argv[2];
+var myoption =process.argv[3] ;
+var artistName;
 
 
-
-inquirer
-.prompt([
-    {
-        type: "list",
-        message: "Please Make a selection",
-        choices: ["concert-this", "spotify-this-song", "movie-this", "do-what-it-says"],
-        name: "mychoice"
-    },
-    {
-        type: "input",
-        message: "Enter Name ",
-        name: "artist"
-    }
-])
-.then(function (inquirerResponse) {
-
-    if(inquirerResponse.mychoice === "concert-this"){
-        concertThis(inquirerResponse.mychoice);
-    }
-    else{
-        concertThis(inquirerResponse.mychoice);
-    }
-
-});
-
-function concertThis(choice){
-console.log(choice);
-axios.get("https://rest.bandsintown.com/artists/" + choice.artist + "/events?app_id=codingbootcamp")
-.then(
-  function(response) {
-console.log(response);
-  console.log("Name of Venue : " + response.venue.name);
-  console.log("Venue Location " + response.venue.city + ' ' + response.venue.region);
-  console.log("Date of Event" );
-  })
-//otherwise log error - Follow the docs. 
-.catch(function (error) {
-    console.log('Hello')
-    console.log(error);
-  }
-)
+if( process.argv[2] === undefined && process.argv[3] === undefined ){
+    console.log("Please make one of the following selections");
+    console.log("concert-this <artist> or spotify-the-song <song Title> or  movie <movie Name> ");
 }
+    switch(myChoice){
+        case "concert-this":
+            concertThis();
+            console.log("Concert");
+            break;
+        
+        case "spotify-this-song":
+            spotifyThatSong();
+            break;
+
+        case "movie-this":
+
+        case "do-what-it-says":
+    }
+
+    function getMyOptions(){
+        if(myChoice === "spotify-this-song")
+        {
+            for(i=4;i < myargs.length;i++){
+                myoption+= " " + myargs[i];
+            }
+        }
+        else{
+            for(i=4;i < myargs.length;i++){
+                    myoption+= "+" + myargs[i];
+            }
+        }
+        return myoption;
+    }
+    
+    function concertThis() {
+    
+           myoption = getMyOptions();
+          
+            if(myoption === undefined || myoption ===''){
+                 console.log("node liri.js concert-this <artist/ band name>")
+            }
+            else{             
+                axios.get("https://rest.bandsintown.com/artists/" + myoption + "/events?app_id=codingbootcamp")
+                .then( function(response){
+                   // console.log(response);
+                    response.data.forEach(element => {
+                        console.log("Name of Venue : " + element.venue["name"]);
+                        console.log("Venue Location " + element.venue["city"] + ' ' + element.venue["region"]);
+                        console.log("Date of Event " + moment(element.datetime).format("MM/DD/YYYY"));
+            
+                    });    
+                });
+            }
+        }
+
+     function spotifyThatSong(){
+
+        var spotify = new Spotify(keys.spotify);
+   
+        if(process.argv[3] ===undefined){
+            myoption = "The Sign";
+        }
+        else{
+            myoption = getMyOptions();
+        }
+        
+         spotify.search({type: 'track' , query: myoption}, function(e, data){
+            if(e){
+                return console.log("Error occured: " + e);
+            }
+            console.log(data.tracks.items.length);
+            for(i=0;i< data.tracks.items.length;i++) {
+                 if(data.tracks.items[i].name === myoption){
+    
+                     var nameObj = data.tracks.items[i].album.Artists;
+                     Object.keys(nameObj).forEach(function(key){
+                         if(key === 'name'){
+                              artistName = nameObj[key];
+                         }
+                     })
+                     console.log("Artists: ", artistName);
+                     console.log("Preview: ",data.tracks.items[i].preview_url);
+                 }
+            }
+         //console.log( data.tracks.items);
+        });
+     }
+    
+     // function movie(){
+    //     axios.get(http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy).
+    //     then(
+    //         function(response){
+    //             console.log("The movie's rating is: " , response);
+    //         }
+    //     ):w
+    
